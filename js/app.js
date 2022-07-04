5cfc5bcb10492b61c82561e9be414f10ecce53c7
Asim Can Yagiz <meycasim@gmail.com>
2022-07-04 23:04:23 +0300

    eventListener builded

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// UI elements
+const taskDOM = document.querySelector("#task");
+const listDOM = document.querySelector("#list");
+const btnDOM = document.querySelector("#liveToastBtn");
+
+///First we need the get all events with eventlistener
+//Event Listener
+eventListener();
+function eventlistener(){
+
+    //Task input
+    taskDOM.addEventListener('keyup',)
+    //List click
+    listDOM.addEventListener('click',)
+    //Ekle Button Click
+    btnDOM.addEventListener('click',)
+}
