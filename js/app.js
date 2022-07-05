8a119eb356e86a16535736a58bf5be090c6980d5
Asim Can Yagiz <meycasim@gmail.com>
2022-07-05 22:21:25 +0300

    Function builded Remove tasks from Local storage

--- js/app.js
+++ js/app.js
@@ -24,7 +24,11 @@ function newElement() {
 
     const li = document.createElement('li')
     li.className = "list-group-item d-flex justify-content-between align-items-center";
-    li.innerHTML = `${taskDOM.value} ${'<i class="bi bi-x iClose"></i>'}`;
+    li.appendChild(document.createTextNode(task.value))
+    const link = document.createElement('a')
+    link.innerHTML = `${'<i class="bi bi-x iClose"></i>'}`
+    li.append(link)
+    listDOM.appendChild(li)
     
     if(taskDOM.value === ''){
         console.log("Eksik")
@@ -64,7 +68,11 @@ function getTasks(){
     tasks.forEach(task => {
         const li = document.createElement('li')
         li.className = "list-group-item d-flex justify-content-between align-items-center";
-        li.innerHTML = `${task} ${'<i class="bi bi-x iClose"></i>'}`;
+        li.appendChild(document.createTextNode(task.value))
+        li.innerHTML = `${task} `;
+        const link = document.createElement('a')
+        link.innerHTML = `${'<i class="bi bi-x iClose"></i>'}`
+        li.append(link)
         listDOM.appendChild(li)
     });
 }
@@ -72,8 +80,34 @@ function getTasks(){
 //Function for remove a task
 function removeElement(event){
 
-    if(event.target.parentElement.classList.contains('d-flex')){
-        event.target.parentElement.remove();
+    if(event.target.parentElement.parentElement.classList.contains('list-group-item')){
+
+        event.target.parentElement.parentElement.remove();
+        removeLocalStorage(event.target.parentElement.parentElement);
+
     }
+
+}
+
+//Function for remove elements from local storage
+function removeLocalStorage(taskItem){
+    
+    let tasks;
+    
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+
+
+    tasks.forEach(function(task, index){
+        console.log(task, index)
+        if(taskItem.textContent == task){
+            tasks.splice(index, 1);
+        }
+    });
+    
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 
 }
